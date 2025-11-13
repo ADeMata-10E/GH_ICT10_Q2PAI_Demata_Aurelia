ea18b23763472c07cd1ef278587447db80ca1ead
adm <demata.aa@obmontessori.edu.ph>
2025-11-13 08:57:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ display(a.union(m), target='output')  #all students
 display(a & m, target='output')  #students who take both art and math
 display(a | m, target='output')  #students who take either art or math
 
+display("Art Club", target="output")
 display(a, target='output')
+
+display("Math Club", target="output")
 display(m, target='output')
